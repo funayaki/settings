8f7d65a28f601d5d91cd07b2ad6bbc8c234ed4d1
Rachman Chavik <rchavik@xintesa.com>
2016-10-07 15:42:07 +0700

    Rename seed files

---
+++ config/Seeds/LanguagesSeed.php
@@ -0,0 +1,21 @@
+<?php
+namespace Croogo\Install\Config\Data;
+
+class LanguageData
+{
+
+    public $table = 'languages';
+
+    public $records = [
+        [
+            'id' => '1',
+            'title' => 'English',
+            'native' => 'English',
+            'alias' => 'eng',
+            'status' => '1',
+            'weight' => '1',
+            'updated' => '2009-11-02 21:37:38',
+            'created' => '2009-11-02 20:52:00'
+        ],
+    ];
+}
